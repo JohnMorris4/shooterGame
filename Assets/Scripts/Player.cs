97f374a72d3c776010122cd0a6d8ac028a65ad17
johnmorris4 <johnmorris4@bellsouth.net>
2019-09-12 06:55:13 -0400

    Addding laser to game

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,7 +6,8 @@ public class Player : MonoBehaviour
 {
     [SerializeField]
     private float _speed = 3.5f;
-    
+    [SerializeField]
+    private GameObject _laserPrefab;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,28 +20,24 @@ public class Player : MonoBehaviour
     void Update()
     {
         CalculateMovement();
+        if(Input.GetKeyDown(KeyCode.Space)){
+            Instantiate(_laserPrefab, transform.position, Quaternion.identity);
+        }
     }
     void CalculateMovement(){
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
         Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
         transform.Translate(direction * _speed * Time.deltaTime);
-        if (transform.position.y >= 3.9f)
-        {
-            transform.position = new Vector3(transform.position.x, 3.9f, 0);
-        }
-        else if (transform.position.y <= -3.8f)
-        {
-            transform.position = new Vector3(transform.position.x, -3.8f, 0);
-        }
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -3.8f, 0), 0);
 
-        if (transform.position.x > 16.77f)
+        if (transform.position.x > 11.3f)
         {
-            transform.position = new Vector3(-16.77f, transform.position.y, 0);
+            transform.position = new Vector3(-11.3f, transform.position.y, 0);
         }
-        else if (transform.position.x < -16.77f)
+        else if (transform.position.x < -11.3f)
         {
-            transform.position = new Vector3(16.77f, transform.position.y, 0);
+            transform.position = new Vector3(11.3f, transform.position.y, 0);
         }
     }
 }
